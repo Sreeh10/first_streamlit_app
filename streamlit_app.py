60a5139418b9e30124d37a3fda63ae7bce1af1af
Sreeh10 <82865021+Sreeh10@users.noreply.github.com>
2023-10-31 14:47:09 +0530

    Update streamlit_app.py
    added code to display the fruit table

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,13 @@
 import streamlit
+import pandas
 
 streamlit.title("My Daily Workout!!")
 streamlit.text( "Run atleast 5k")
 streamlit.text( "Do some basic gym things...")
 streamlit.text( "Be sure to wake up early" )
 
-streamlit.header ("What should I plan Next ?")
+streamlit.header ("What should I plan Next ? I should Eat some fruits")
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+streamlit.dataframe(my_fruit_list)
