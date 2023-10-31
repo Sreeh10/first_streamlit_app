1a6ff1f7321ba5b6b369619db1cd87f7cdfe8b63
Sreeh10 <82865021+Sreeh10@users.noreply.github.com>
2023-10-31 14:44:21 +0530

    Update streamlit_app.py
    trying out header

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,3 +4,5 @@ streamlit.title("My Daily Workout!!")
 streamlit.text( "Run atleast 5k")
 streamlit.text( "Do some basic gym things...")
 streamlit.text( "Be sure to wake up early" )
+
+streamlit.header ("What should I plan Next ?")
